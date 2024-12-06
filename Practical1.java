b0bec7244e454952d3567c560b22c214a49b091b
ramya777-dr <ramyadhara68@gmail.com>
2024-12-06 04:57:13 +0000

    commit1

--- Practical1.java
+++ Practical1.java
@@ -3,19 +3,17 @@ class Person
     String firstName;
     String  lastName;
     int age;
-    
     public Person()
     {
         firstName = "";
         lastName  = "";
         age = 0;
     }
-    public Person(String firstName, String lastName, int age) {
+       public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
-
    public String getFullName(){
    return firstName + "" + lastName;
 }
@@ -24,12 +22,11 @@ public class Practical1
 {
 public static void main(String[]args)
 {
-
 Person Person1 = new Person(" John"," Doe",30);
 Person Person2 = new Person(" Alice"," Smith",25);
 System.out.println("Person 1:" + Person1.getFullName());
-System.out.println("Person 2:" + Person2.getFullName());
-double averageAge = (Person1.age + Person2.age) / 2.0;
-System.out.println("Average Age:" + averageAge);
+ System.out.println("Person 2:" + Person2.getFullName());
+ double averageAge = (Person1.age + Person2.age) / 2.0;
+ System.out.println("Average Age:" + averageAge);
      }
 }
